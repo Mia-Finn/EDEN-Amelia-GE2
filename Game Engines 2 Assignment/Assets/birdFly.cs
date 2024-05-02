1e630e757e25f9408824b60f16920e92dededd0f
Mia-Finn <92232844+Mia-Finn@users.noreply.github.com>
2024-05-02 14:42:27 +0100

    Working more on bird wings

---
+++ Game Engines 2 Assignment/Assets/birdFly.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class birdFly : MonoBehaviour
+{
+    public float speed;
+    public GameObject target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //fly towards a target
+        Vector3 targetPos = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        transform.position = targetPos;
+        Debug.Log("Moving!");
+    }
+}
