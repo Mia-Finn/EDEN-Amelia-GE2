e4a4e52551a60a580258439185a657bcf9d2cfe7
Mia-Finn <92232844+Mia-Finn@users.noreply.github.com>
2024-05-03 22:05:49 +0100

    Working on summer scene

--- Game Engines 2 Assignment/Assets/Scenes/Scripts/textController.cs
+++ Game Engines 2 Assignment/Assets/Scenes/Scripts/textController.cs
@@ -6,9 +6,9 @@ using TMPro;
 
 public class textController : MonoBehaviour
 {
-    public GameObject player, BB, BT, R, CF, HS, GF, Book;
+    public GameObject player, BB, BT, R, CF, HS, GF, S, Book;
     public TMP_Text typeText, habitatText, factText, interactText;
-    private bool isBB, isBT, isR, isCF, isHS, isGF, isBook;
+    private bool isBB, isBT, isR, isCF, isHS, isGF, isS, isBook;
     // public string None;
 
     private void Update()
@@ -20,6 +20,7 @@ public class textController : MonoBehaviour
         isCF = Vector3.Distance(player.transform.position, CF.transform.position) < 3f; //Chaffinch
         isHS = Vector3.Distance(player.transform.position, HS.transform.position) < 3f; //House Sparrow
         isGF = Vector3.Distance(player.transform.position, GF.transform.position) < 3f; //Goldfinch
+        isS = Vector3.Distance(player.transform.position, S.transform.position) < 3f; //Starling
         isBook = Vector3.Distance(player.transform.position, Book.transform.position) < 3f; //Book menu open
 
         if (isBT)
@@ -28,7 +29,6 @@ public class textController : MonoBehaviour
             habitatText.text = "Habitat: Gardens & Farmland";
             factText.text = "Fun Fact: Considered a European bird even though they are found in most of the world";
             interactText.text = " ";
-            //    Debug.Log("Is Blue Tit");
         }
         else if(isBB)
         {
@@ -36,31 +36,27 @@ public class textController : MonoBehaviour
             habitatText.text = "Habitat: Gardens & Farmland";
             factText.text = "Fun Fact: Males are black with orange beaks, females are brown";
             interactText.text = " ";
-            //    Debug.Log("Is Blackbird");
         }
         else if (isR)
         {
-            typeText.text = "Bird Type: Blackbird";
+            typeText.text = "Bird Type: Robin";
             habitatText.text = "Habitat: Gardens & Farmland";
-            factText.text = "Fun Fact: Males are black with orange beaks, females are brown";
+            factText.text = "Fun Fact: Robins are very territorial and will defend their land with their lives";
             interactText.text = " ";
-            //  Debug.Log("Is Blackbird");
         }
         else if (isCF)
         {
-            typeText.text = "Bird Type: Goldfinch";
+            typeText.text = "Bird Type: Chaffinch";
             habitatText.text = "Habitat: Gardens & Farmland";
-            factText.text = "Fun Fact: Many Goldfinches migrate in the Winter";
+            factText.text = "Fun Fact: These birds have been found to have regional accents depending on the country they live in";
             interactText.text = " ";
-            //    Debug.Log("Is Goldfinch");
         }
         else if (isHS)
         {
-            typeText.text = "Bird Type: Blackbird";
+            typeText.text = "Bird Type: House Sparrow";
             habitatText.text = "Habitat: Gardens & Farmland";
-            factText.text = "Fun Fact: Males are black with orange beaks, females are brown";
+            factText.text = "Fun Fact: House Sparrows won't migrate in winter, they will just move to the countryside to feed on grain during the autumn harvest";
             interactText.text = " ";
-            //Debug.Log("Is Goldfinch");
         }
         else if (isGF)
         {
@@ -68,7 +64,13 @@ public class textController : MonoBehaviour
             habitatText.text = "Habitat: Gardens & Farmland";
             factText.text = "Fun Fact: Many Goldfinches migrate in the Winter";
             interactText.text = " ";
-            //  Debug.Log("Is Goldfinch");
+        }
+        else if (isS)
+        {
+            typeText.text = "Bird Type: Starling";
+            habitatText.text = "Habitat: Gardens & Farmland";
+            factText.text = "Fun Fact: Starlings have the ability to mimic other bird calls or even machine noises";
+            interactText.text = " ";
         }
         else if (isBook)
         {
@@ -77,7 +79,7 @@ public class textController : MonoBehaviour
             factText.text = " ";
             interactText.text = "Press 'E' to interact!";
         }
-        else if(!isGF && !isCF && !isBT && !isBB && !isHS && !isR && !isBook)
+        else if(!isGF && !isCF && !isBT && !isBB && !isHS && !isR && !isS && !isBook)
         {
             typeText.text = " ";
             habitatText.text = " ";
