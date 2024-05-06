e45b8148a242b052f55f0badd5417b58f01e9560
Mia-Finn <92232844+Mia-Finn@users.noreply.github.com>
2024-05-06 21:23:39 +0100

    Particle system added

--- Game Engines 2 Assignment/Assets/Scripts/sceneSwitch.cs
+++ Game Engines 2 Assignment/Assets/Scripts/sceneSwitch.cs
@@ -9,27 +9,35 @@ public class sceneSwitch : MonoBehaviour
     public void summerScene()
     {
         SceneManager.LoadScene("Summer scene");
-        Debug.Log("Summer Loaded!");
+     //   Debug.Log("Summer Loaded!");
     }
 
     //switcch to spring scene
     public void springScene()
     {
         SceneManager.LoadScene("Spring scene");
-        Debug.Log("Spring Loaded!");
+     //   Debug.Log("Spring Loaded!");
     }
 
     //switch to winter scene
     public void winterScene()
     {
         SceneManager.LoadScene("Winter scene");
-        Debug.Log("Winter Loaded!");
+      //  Debug.Log("Winter Loaded!");
     }
 
     //switch to autumn scene
     public void autumnScene()
     {
         SceneManager.LoadScene("Autumn scene");
-        Debug.Log("Autumn Loaded!");
+      //  Debug.Log("Autumn Loaded!");
     }
+
+    //Exit game
+    public void exitGame()
+    {
+        Application.Quit();
+        Debug.Log("QUIT GAME!");
+    }
+
 }
